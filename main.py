6db199382b598f39ed47685ba21db3117e4f5873
hyeonukim <48634064+hyeonukim@users.noreply.github.com>
2021-05-13 19:03:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -52,72 +52,55 @@ async def racoon(ctx):
     await ctx.send('Racoon man is protecting you!', file=discord.File('Desktop/racoon/racoon.jpg'))
 
 
-# @client.command()
-# async def play(ctx, url: str):
-#     song_there = os.path.isfile('song.mp3')
-#     try:
-#         if song_there:
-#             os.remove('song.mp3')
-#     except PermissionError:
-#         await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-#         return
 
-         
-#     voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='추노')
-#     await voiceChannel.connect()
-#     voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+@client.command()
+async def play(ctx,url):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
 
+    await channel.connect()
 
-#     ydl_opts = {
-#         'format': 'bestaudio/best',
-#         'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-#         'restrictfilenames': True,
-#         'noplaylist': True,
-#         'nocheckcertificate': True,
-#         'ignoreerrors': False,
-#         'logtostderr': False,
-#         'quiet': True,
-#         'no_warnings': True,
-#         'default_search': 'auto',
-#         'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-#     }
+    server = ctx.message.guild
+    voice_channel = server.voice_client
 
-#     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#         ydl.download([url])
-#     for file in os.listdir("./"):
-#         if file.endswith('.mp3'):
-#             os.rename(file, 'song.mp3')
-#     voice.play(discord.FFmpegPCMAudio('song.mp3'))
+    async with ctx.typing():
+        filename = await YTDLSource.from_url(url, loop=client.loop)
+        voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+    await ctx.send('**Now playing:** {}'.format(filename))
 
 
-# @client.command()
-# async def leave(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-#     if voice.is_connected():
-#         await voice.disconnect()
-#     else:
-#         await ctx.send("Racoon man stopped the music for you!")
 
-# @client.command()
-# async def pause(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-#     if voice.is_playing():
-#         voice.pause()
-#     else:
-#         await ctx.send('STOP!')
+@client.command()
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("Racoon man stopped the music for you!")
 
-# @client.command()
-# async def resume(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-#     if voice.is_paused():
-#         voice.resumem()
-#     else:
-#         await ctx.send('PLAY!')
+@client.command()
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send('STOP!')
 
-# @client.command()
-# async def stop(ctx):
-#     voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-#     voice.stop()
+@client.command()
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    if voice.is_paused():
+        voice.resumem()
+    else:
+        await ctx.send('PLAY!')
+
+@client.command()
+async def stop(ctx):
+    voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+    voice.stop()
 
 
 client.run('TOKEN')
